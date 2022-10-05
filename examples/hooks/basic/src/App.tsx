c3e2c3a54a40ca4649f5774d587856fe643b735f
yasir900aslam <yasir900aslam@gmail.com>
2022-10-06 03:26:50 +0500

    Version fixes

--- examples/hooks/basic/src/App.tsx
+++ examples/hooks/basic/src/App.tsx
@@ -1,23 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { useDriver } from "driver.jsx";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-
+import "driver.jsx/dist/style.css";
 function App() {
   const [count, setCount] = useState(0);
+  const basicRef = useRef<HTMLAnchorElement>(null);
+  const { driver, isActivated } = useDriver({
+    allowClose: false,
+  });
 
   useEffect(() => {
-    // driver.highlight({
-    //   element: ref.current,
-    //   popover: {
-    //     title: "Title for the Popover",
-    //     description: "Description for it",
-    //   },
-    // });
+    if (basicRef && isActivated) {
+      driver.highlight({
+        element: basicRef.current,
+        popover: {
+          title: "Title for the Popover",
+          description: "Description for it",
+        },
+      });
+    }
   });
+
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a ref={basicRef} href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,33 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
+import { EsLinter, linterPlugin } from "vite-plugin-linter";
 
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import dts from 'vite-plugin-dts'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
-
-export default defineConfig(configEnv => ({
+export default defineConfig((configEnv) => ({
   plugins: [
     react(),
     tsConfigPaths(),
     linterPlugin({
-      include: ['./src}/**/*.{ts,tsx}'],
+      include: ["./src}/**/*.{ts,tsx}"],
       linters: [new EsLinter({ configEnv })],
     }),
     dts({
-      include: ['lib/main.ts'],
+      include: ["./types.ts"],
       beforeWriteFile: (filePath, content) => ({
-        filePath: filePath.replace('/lib', ''),
         content,
       }),
     }),
   ],
   build: {
     lib: {
-      entry: resolve('lib', 'main.ts'),
-      name: 'driver.jsx',
+      entry: resolve("lib", "main.ts"),
+      name: "driver.jsx",
       fileName: (format) => `react-driver-${format}.js`,
     },
     rollupOptions: {
-      external: ['react'],
+      external: ["react"],
     },
   },
-}))
+}));
